1ada1216378f78533da9401b72c0f35fd90ce4cd
AnatoliyKotok <61416716+AnatoliyKotok@users.noreply.github.com>
2020-11-19 13:47:00 +0200

    Add files via upload

---
+++ FileWorkerHomeWork/FileWorkerHomeWork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWorkerHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname = "Student.txt";
+            Student student = new Student("Anatoliy", "Kotok", new int[] { 11, 12, 11 }, ProfesionType.Programmer);
+            Console.WriteLine(student);
+            Student student2 = new Student("Bob", "Kotok", new int[] { 11, 12, 11 }, ProfesionType.Designer);
+            FileWorker.SaveStudent(student, fname);
+            FileWorker.SaveStudent(student2, fname);
+            FileWorker.LoadStudent(fname);
+        }
+    }
+}
---
+++ FileWorkerHomeWork/FileWorkerHomeWork/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWorkerHomeWork
+{
+    enum ProfesionType {Designer, Programmer };
+    class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int[] Marks { get; set; }
+        public ProfesionType Profesion { get; set; }
+        public Student(string name,string surname,int[]marks,ProfesionType profesion)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Marks = marks;
+            this.Profesion = profesion;
+        }
+        public override string ToString()
+        {
+            return $"{Name}\n{Surname}\n{String.Join(" ",Marks)}\n{Profesion}";
+        }
+
+    }
+
+    class FileWorker
+    {
+        public static void SaveStudent(Student student, string fname)
+        {
+            using (StreamWriter sw = new StreamWriter(fname, true, System.Text.Encoding.Default))
+            {
+                
+                sw.WriteLine(student);
+            }
+        }
+        public static void LoadStudent(string fname)
+        {
+            using (StreamReader sr = new StreamReader(fname, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line); 
+                    
+                    //Student student;
+                    //string[] items = line.Split(' '); 
+                }
+            }
+        }
+    }
+}
